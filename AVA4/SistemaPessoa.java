1beea762c9a08cfea4dc360f8f95adb3c2fc73b4
gabrielvloiola <167367911+gabrielvloiola@users.noreply.github.com>
2024-11-13 09:31:22 -0300

    SistemaPessoa.java
    AVA4

---
+++ AVA4/SistemaPessoa.java
@@ -0,0 +1,9 @@
+/*
+    Classe de inicializa a aplicação.
+ */
+public class SistemaPessoa {
+    public static void main(String[] args) {
+        // Inicia a aplicação com o menu principal
+        new MenuPrincipal();
+    }
+}
